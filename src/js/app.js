29b88eb67e62e3847cd6029de0dddc6a97f3e60c
Frederick Lopez <mtester010@gmail.com>
2022-04-08 22:07:22 -0500

    Add search functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,47 @@
 // setup app
 
-const movieContainer = document.querySelector('.movie-container');
+const movieContainer = document.querySelector('.movie-thumb');
 
 movieContainer.addEventListener('mouseover', (e) => {
-  e.target.closest('ul li').lastElementChild.style.display = "block";
+    e.target.closest('li').lastElementChild.style.display = "block";
 })
 
 movieContainer.addEventListener('mouseout', (e) => {
-  e.target.closest('ul li').lastElementChild.style.display = "none";
+    e.target.closest('li').lastElementChild.style.display = "none";
 })
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userSearch = document.querySelector('#search-pane').value
+  getMovie(userSearch)
+  .then((movies) => {
+    movieContainer.textContent ="";
+    movies.forEach(movie => {
+      movieContainer.insertAdjacentHTML(
+        'beforeend',
+        `
+        <li class="movie">
+          <img src="${movie.Poster}" alt="">
+          <div class="details">
+            <h2 class="title">${movie.Title}</h2>
+            <h3 class="rating">Rating: 5.4/10</h3>
+            <p class="plot">
+              "${movie.Plot}"
+            </p>
+          </div>
+        </li>
+        `
+      ) 
+    })
+  })
+})
+
+const getMovie = (userSearch) => {
+  return fetch(`http://www.omdbapi.com/?apikey=678105a7&s=${userSearch}`)
+  .then((results) => 
+    results.json()
+  )
+  .then((data) => data.Search);
+}
