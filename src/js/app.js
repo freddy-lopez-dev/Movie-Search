650b0eb91953a5fa128bd82402e66514f412ee35
Frederick Lopez <mtester010@gmail.com>
2022-04-08 21:29:37 -0500

    Add hover details on each thumbnail

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
 // setup app
+
+const movieContainer = document.querySelector('.movie-container');
+
+movieContainer.addEventListener('mouseover', (e) => {
+  e.target.closest('ul li').lastElementChild.style.display = "block";
+})
+
+movieContainer.addEventListener('mouseout', (e) => {
+  e.target.closest('ul li').lastElementChild.style.display = "none";
+})
