4e6e5c638b57bbd0834f7d75f61544ae6caeb586
Frederick Lopez <mtester010@gmail.com>
2022-04-11 15:23:40 -0500

    Add pagination for search result

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,3 @@
-// setup app
-
 const movieContainer = document.querySelector('.movie-thumb');
 
 movieContainer.addEventListener('mouseover', (e) => {
@@ -10,23 +8,32 @@ movieContainer.addEventListener('mouseout', (e) => {
     e.target.closest('li').lastElementChild.style.display = "none";
 })
 
-const form = document.querySelector('form')
+const getMovie = (userSearch, currentPage) => {
+  return fetch(`https://www.omdbapi.com/?apikey=678105a7&s=${userSearch}&page=${currentPage}`)
+  .then((results) => 
+    results.json()
+  )
+  .then((data) => data.Search);
+}
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const userSearch = document.querySelector('#search-pane').value
-  getMovie(userSearch)
-  .then((movies) => {
-    movieContainer.textContent ="";
-    movies.forEach(movie => {
+const getMovieDetails = (movieId) => {
+  return fetch(`https://www.omdbapi.com/?apikey=678105a7&i=${movieId}`)
+  .then(resp => resp.json())
+}
+
+const populateMovieList = (movies) => {
+  movieContainer.textContent ="";
+  movies.forEach(movie => {
+    getMovieDetails(movie.imdbID)
+    .then((movie) =>{
       movieContainer.insertAdjacentHTML(
         'beforeend',
         `
         <li class="movie">
-          <img src="${movie.Poster}" alt="">
+          <img src="${movie.Poster === 'N/A' ? "images/freddy-flix.png" : movie.Poster}" alt="">
           <div class="details">
             <h2 class="title">${movie.Title}</h2>
-            <h3 class="rating">Rating: 5.4/10</h3>
+            <h3 class="rating">Rating: ${movie.imdbRating}/10</h3>
             <p class="plot">
               "${movie.Plot}"
             </p>
@@ -36,12 +43,64 @@ form.addEventListener('submit', (e) => {
       ) 
     })
   })
-})
+}
 
-const getMovie = (userSearch) => {
+const handleSearch = (event) => {
+  event.preventDefault();
+  const userSearch = document.querySelector('#search-pane').value
+  const pagination = document.querySelector('.pagination');
+  const prev = document.querySelector('.prev');
+  const next = document.querySelector('.next');
+  let currentPage = 1;
+  getTotalResults(userSearch)
+  .then((data) => {
+    if (data < 12) {
+      pagination.style.display = "none";
+    }
+    
+    if (data > 12) {
+      pagination.style.display = "flex";
+      prev.style.display = "none";
+      next.style.display = "block";
+    }    
+  })
+  getMovie(userSearch, currentPage)
+  .then((movies) => populateMovieList(movies));
+
+  next.addEventListener('click', () => {
+    currentPage++;
+    prev.style.display = "block";
+    getMovie(userSearch, currentPage)
+    .then((movies) => populateMovieList(movies));
+    console.log(currentPage);
+  });
+
+  prev.addEventListener('click', () => {
+    currentPage--;
+    if (currentPage === 1) {
+      prev.style.display = "none";
+    }
+    getMovie(userSearch, currentPage)
+    .then((movies) => populateMovieList(movies));
+    console.log(currentPage);
+  });
+}
+
+const getTotalResults = (userSearch) => {
   return fetch(`https://www.omdbapi.com/?apikey=678105a7&s=${userSearch}`)
   .then((results) => 
     results.json()
   )
-  .then((data) => data.Search);
+  .then((data) => data.totalResults);
 }
+
+const form = document.querySelector('form')
+form.addEventListener('submit', handleSearch);
+
+
+// math.ceil(totalResults / 10) > pageNumber
+// page numbers
+// promise.all
+// Search error handling - text input value invalid
+// no image / no rating / no plot give proper message
+// cache search result
